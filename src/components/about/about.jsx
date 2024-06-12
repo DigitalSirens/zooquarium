b7df083e6b807e253903cca31ec4a8a7a937360c
Stephanie <st3phanie.harris@gmail.com>
2024-06-11 20:17:56 -0400

    fix: added login button and styled the header appropriately

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const about = () => {
+const About = () => {
   return (
     <div>
       <h1>Placeholder Component</h1>
@@ -12,4 +12,4 @@ const about = () => {
   );
 };
 
-export default about;
+export default About;
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,26 +5,26 @@ const Header = () => {
   return (
     <header className='header'>
       <div className='header-container'>
-        <h1 className='header-title'>Zooquarium</h1>
-        <nav className='header-nav'>
-          <ul className='header-nav-list'>
-            <li className='header-nav-item'>
-              <a href='#home' className='header-nav-link'>
-                Home
-              </a>
-            </li>
-            <li className='header-nav-item'>
-              <a href='#about' className='header-nav-link'>
-                About
-              </a>
-            </li>
-            <li className='header-nav-item'>
-              <a href='#contact' className='header-nav-link'>
-                Contact
-              </a>
-            </li>
-          </ul>
-        </nav>
+        <div className='header-left'>
+          <h1 className='header-title'>Zooquarium</h1>
+        </div>
+        <div className='header-right'>
+          <nav className='header-nav'>
+            <ul className='header-nav-list'>
+              <li className='header-nav-item'>
+                <a href='about' className='header-nav-link'>
+                  About
+                </a>
+              </li>
+              <li className='header-nav-item'>
+                <a href='contact' className='header-nav-link'>
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <button className='login-button'>Login</button>
+        </div>
       </div>
     </header>
   );
