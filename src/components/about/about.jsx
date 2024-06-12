dba065d5b239ef5b058351c56cffd7f3a7345be0
Stephanie <st3phanie.harris@gmail.com>
2024-06-11 20:55:41 -0400

    fix: added 'about' in component placeholder

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
   return (
     <div>
-      <h1>Placeholder Component</h1>
+      <h1>Placeholder About Component</h1>
       <p>
         This is a placeholder component. Replace it with actual content during
         development.
